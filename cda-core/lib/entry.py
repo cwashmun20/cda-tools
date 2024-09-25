76c7480cb93b5f3c55f35108061633aa2e52d04d
Clifford Ashmun <cliffordashmun@gmail.com>
2024-09-24 18:41:45 -0700

    Moved self add to partnership event lists to end of constructor to avoid potential data loss.

--- cda-core/lib/entry.py
+++ cda-core/lib/entry.py
@@ -12,9 +12,9 @@ class Entry:
     def __init__(self, dance_obj: dance.Dance, dancers: partners.Partnership, event_name, heat=None):
         self.dance_data = dance_obj
         self.partnership = dancers
-        self.partnership.add(self)
         self.event_name = event_name
         self.heat = heat
+        self.partnership.add(self)
 
     def __repr__(self):
         return str(self.dance_data)
